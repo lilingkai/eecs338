5f91489f99add88f123d7a75b8e9567058907d30
lilingkai <li.kevin295@gmail.com>
2017-11-13 00:21:47 -0600

    Added jquery code to handle progress bar

--- js/grading.js
+++ js/grading.js
@@ -1,71 +1,21 @@
 (function() {
     $(function() {
-        attachEntryHandlers();
-
-        $('#budget-limit').keypress(function(e) {
-            if (e.which === 13) {
-                e.preventDefault();
-                var budget = $(this).val();
-                $(this).parents('div:first')
-                    .replaceWith('<h2>' + 'Budget Limit: $' +  budget + '</h2>');
+        if(typeof(Storage) !== "undefined") {
+            if (sessionStorage.progress) {
+                $('#number > span').text(Number(sessionStorage.progress) + 1);
+            }
+        }
+        $('#submit').click(function(e) {
+            if(typeof(Storage) !== "undefined") {
+                if (sessionStorage.progress) {
+                    sessionStorage.progress = Number(sessionStorage.progress) + 1;
+                    if (sessionStorage.progress > 100) {
+                        sessionStorage.progress = 0;
+                    }
+                } else {
+                    sessionStorage.progress = 0;
+                }
             }
         });
-
-        $('#add').click(function(e) {
-            $('tbody').append(entryHTML);
-            $('select').material_select();
-            attachEntryHandlers();
-        });
     });
-
-    function attachEntryHandlers() {
-        $('select').material_select();
-
-        $('.save').click(function(e) {
-            var $entry = $(this).parents('.entry:first');
-            category = capitalize($entry.find('select').val())
-            name = capitalize($entry.find('input[name="name"]').val())
-            amount = '$' + $entry.find('input[name="amount"]').val()
-
-            var $entry = $(this).parents('.entry:first');
-            $entry.empty();
-            $entry.append('<td>' + category + '</td>' +
-                '<td>' + name + '</td>' +
-                '<td>' + amount + '</td>'
-            );
-        });
-
-        $('.delete').click(function(e) {
-            var $entry = $(this).parents('.entry:first');
-            $entry.remove();
-            console.log('hi')
-        });
-    }
-
-    function capitalize(str) {
-        return str.charAt(0).toUpperCase() + str.slice(1);
-    }
-
-    var entryHTML = [
-        '<tr class="entry">',
-        '    <td>',
-        '        <select>',
-        '            <option value="" disabled selected>Category</option>',
-        '            <option value="Rent">Rent</option>',
-        '            <option value="Food">Food</option>',
-        '            <option value="Recreation">Recreation</option>',
-        '        </select>',
-        '    </td>',
-        '    <td>',
-        '        <input name="name" type="text" placeholder="Name" />',
-        '    </td>',
-        '    <td>',
-        '        <input name="amount" type="number" placeholder="Amount" />',
-        '    </td>',
-        '    <td>',
-        '        <button class="btn btn-flat waves-effect waves-light transparent save"><i class="material-icons positive" type="submit">check</i></button>',
-        '        <button class="btn btn-flat waves-effect waves-light transparent delete"><i class="material-icons negative" type="submit">delete</i></button>',
-        '    </td>',
-        '</tr>'
-    ].join('\n')
 }());
--- js/home2.js
+++ js/home2.js
@@ -1,56 +1,25 @@
 (function() {
-    //$(function() {
-        //$('.progress > .determinate').css('width', '0%')
-    //})
-
     $(function() {
-        $('select').material_select();
-        var budgetLimit = parseInt($('#limit').text());
+        
+        updateProgressBar();
 
-        $('#new-expenditure-form').submit(function(e) {
-            e.preventDefault();
-
-            var category = $(this).find('#new-expenditure-category').val();
-            var amount = parseInt($(this).find('#new-expenditure').val());
-
-            updateProgressBar(category, amount);
-            updateProgressBar('total', amount)
-
-            $('#new-expenditure').val('');
-        });
     });
 
-    function updateProgressBar(category, amount) {
-        var categoryLimit = parseInt($('#' + category + '-progress ~ .right > span').text());
-        var currentSavings = parseInt($('label[for="' + category + '-progress"] > span').text());
+    function updateProgressBar() {
+        var total = 100;
+        var current = 0;
+        if(typeof(Storage) !== "undefined") {
+            if (sessionStorage.progress) {
+                current = sessionStorage.progress;
+                $('label[for="progress"] > span').text(current);
+            } else {
+                sessionStorage.progress = 0;
+            }
+        }
 
-        currentSavings -= amount;
-        $('label[for="' + category + '-progress"] > span').text(currentSavings);
-
-        var percent = 100 * currentSavings / categoryLimit;
-
-        console.log(category)
-
-        var $progressBarBackground = $('#' + category + '-progress.progress');
+        var $progressBarBackground = $('#progress.progress');
         var $progressBar = $progressBarBackground.find('.determinate');
-        $progressBar.css('width',  percent + '%');
-        if (percent < 20) {
-            $progressBarBackground.removeClass('green lighten-3');
-            $progressBarBackground.removeClass('yellow lighten-4');
-            $progressBarBackground.addClass('red lighten-3');
-
-            $progressBar.removeClass('green');
-            $progressBar.removeClass('yellow darken-1');
-            $progressBar.addClass('red');
-        }
-        else if (percent < 50) {
-            $progressBarBackground.removeClass('green lighten-3');
-            $progressBarBackground.removeClass('yellow lighten-4');
-            $progressBarBackground.addClass('yellow lighten-4');
-
-            $progressBar.removeClass('green');
-            $progressBar.removeClass('red');
-            $progressBar.addClass('yellow darken-1');
-        }
+        console.log($progressBar)
+        $progressBar.css('width',  current + '%');
     }
 }());
