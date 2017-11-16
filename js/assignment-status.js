0314d87a5fb142762d182bf1edd84ec5a38b5bf6
lilingkai <li.kevin295@gmail.com>
2017-11-16 08:45:58 -0600

    fleshed out ta-status and assignment status pages for professor view

---
+++ js/assignment-status.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+        datasets: [{
+            label: "Current Grade Distribution",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [1, 0, 2, 2, 5, 8, 15, 30, 25, 12, 8],
+        }]
+    },
+
+    // Configuration options go here
+        options: {}
+    });
+}
+
